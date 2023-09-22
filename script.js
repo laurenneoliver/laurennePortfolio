19669c2a44305a7afa28bd32b6b5c17f216e15d7
Laurenne <laurenneoliver@gmail.com>
2023-09-21 22:09:40 -0500

    fixed rhe overlay

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 const navBar = document.querySelector("nav");
 const menuBtns = document.querySelectorAll(".menu-icon");
 const overlay = document.querySelector(".overlay");
+const logo = document.querySelector(".logo");
+const openbar = document.querySelector(".open");
 
-menuBtns.forEach((menuBtns) => {
+  menuBtns.forEach((menuBtns) => {
     menuBtns.addEventListener("click", () => {
         navBar.classList.toggle("open");
+        if (overlay.style.display === "none") {
+            overlay.style.display = "block";
+        } else {
+            overlay.style.display = "none";
+        }
     });
-});
-
-overlay.addEventListener("click", () => {
-    navBar.classList.remove("open");
 });
 
 
